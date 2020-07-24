4eadf3229d312579846cd69a271140112905c022
Davide Guglielmino <davideguglielmino.programmer@gmail.com>
2020-07-24 23:37:40 +0200

    Initial Unity Commit
    Added Scripts:
    - GitUtilities (Prune)
    - Singleton
    - GameManager
    
    Added Scenes
    - StartingMenu
    - Gym
    
    Added URP files
    
    Added Package Manager Extensions (see packages-lock.json)

---
+++ Assets/Editor/GitUtilities.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using System.IO;
+
+[InitializeOnLoad]
+public static class GitUtilities
+{
+    static GitUtilities()
+    {
+        if (string.IsNullOrEmpty(EditorSceneManager.GetActiveScene().name))
+        {
+            System.Environment.SetEnvironmentVariable(
+                "UNITYYAMLMERGE",
+                EditorApplication.applicationPath.Replace("Unity.exe", "Data/Tools/UnityYAMLMerge.exe"),
+                System.EnvironmentVariableTarget.User);
+
+            Prune();
+            EditorApplication.quitting += Prune;
+        }
+    }
+
+    static bool PruneStep(string path)
+    {
+        bool pruned = false;
+        foreach (string subFolder in Directory.EnumerateDirectories(path))
+        {
+            pruned |= PruneStep(subFolder);
+        }
+
+        if (Directory.GetFiles(path).Length == 0)
+        {
+            Debug.Log("Pruning empty folder " + path);
+            Directory.Delete(path);
+            File.Delete(path + ".meta");
+            return true;
+        }
+        else
+        {
+            return pruned;
+        }
+    }
+
+    [MenuItem("Utilities/Prune Empty Folders")]
+    static void Prune()
+    {
+        if (PruneStep(Application.dataPath))
+        {
+            AssetDatabase.Refresh();
+        }
+        else
+        {
+            Debug.Log("No empty folders to prune.");
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            Debug.Assert(instance != null);
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
