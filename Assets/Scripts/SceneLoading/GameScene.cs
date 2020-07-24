0f803ac515eaa44c8b8f50bab52f6c98b30270bc
Davide Guglielmino <davideguglielmino.programmer@gmail.com>
2020-07-24 23:44:07 +0200

    Created Scene Loading Architecture using Scriptable Object
    For more information visit the link below:
    
    https://blogs.unity3d.com/2020/07/01/achieve-better-scene-workflow-with-scriptableobjects/

---
+++ Assets/Scripts/SceneLoading/GameScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameScene : ScriptableObject
+{
+    [Header("Information")]
+    public SceneReference scene;
+ 
+    [Header("Sounds")]
+    public AudioClip music;
+    [Range(0.0f, 1.0f)]
+    public float musicVolume;
+}
